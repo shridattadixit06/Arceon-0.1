37dd66e503e2aeeb8901b5cab22f3438b764dd83
Shridatta Dixit <shridattadixit.cs25@bmsce.ac.in>
2026-08-15 19:42:44 +0530

    Displayed a string using iteration with help of BIOS video output service

--- boot/boot.asm
+++ boot/boot.asm
@@ -3,22 +3,22 @@ org 0x7c00
 
 start:
     mov ah, 0x0e
-    mov al, 'A'
-    int 0x10   
-    mov al, 'R'
+    mov si, message
+
+my_loop:                
+    mov al, [si]
+    cmp al, 0
+    je hang
+    
     int 0x10
-    mov al, 'C'
-    int 0x10
-    mov al, 'E'
-    int 0x10
-    mov al, 'O'
-    int 0x10
-    mov al, 'N'
-    int 0x10
+    inc si
+    jmp my_loop         
+
+message db 'Arceon', 0
 
 hang:
     hlt
     jmp hang
 
 times 510-($-$$) db 0
-dw 0xaa55
+dw 0xaa55   
