a3547ce683ac933481faf867bd2e881169a67a75
Shridatta Dixit <shridattadixit.cs25@bmsce.ac.in>
2026-08-15 19:29:06 +0530

    Used BIOS video output service to display message

--- boot/boot.asm
+++ boot/boot.asm
@@ -2,7 +2,19 @@ bits 16
 org 0x7c00
 
 start:
-    cli
+    mov ah, 0x0e
+    mov al, 'A'
+    int 0x10   
+    mov al, 'R'
+    int 0x10
+    mov al, 'C'
+    int 0x10
+    mov al, 'E'
+    int 0x10
+    mov al, 'O'
+    int 0x10
+    mov al, 'N'
+    int 0x10
 
 hang:
     hlt
